c99c0b0bfc2c65114a32c8f99eeb130fab40e982
kpndevroot <vishnukpn884@gmail.com>
2023-03-20 17:27:31 +0530

    v.5

---
+++ pages/index.js
@@ -0,0 +1,18 @@
+import Head from "next/head";
+import Header from "../components/Header";
+import Main from "../components/Main";
+import Footer from "../components/Footer";
+
+export default function Home() {
+  return (
+    <div className="text-black">
+      <Head>
+        <title>GiCare</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Header />
+      <Main />
+      <Footer />
+    </div>
+  );
+}
---
+++ tailwind.config.js
@@ -0,0 +1,28 @@
+module.exports = {
+  purge: ["./components/**/*.js", "./pages/**/*.js"],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    extend: {
+      colors: {
+        blue: {
+          primary: "#2732EB",
+        },
+        gray: {
+          100: "#FBFBFB",
+          200: "#EAEAEA",
+          300: "#DFDFDF",
+          400: "#999999",
+          500: "#7F7F7F",
+          600: "#666666",
+          700: "#4C4C4C",
+          800: "#333333",
+          900: "#191919",
+        },
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+};
